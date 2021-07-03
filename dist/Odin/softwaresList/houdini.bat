5582d823dccfe26ab6d941d252b40885407d2ca4
t.nicole <thomas-nicole@hotmail.fr>
2021-07-03 12:32:58 +0200

    test houdin launch

---
+++ dist/Odin/softwaresList/houdini.bat
@@ -0,0 +1,4 @@
+set PYTHONPATH=C:\Program Files\Side Effects Software\Houdini 18.0.532\python27\lib\site-packages;C:\Program Files\Side Effects Software\Houdini 18.0.532\python27\lib\site-packages-forced;%PYTHONPATH%
+set PYTHONPATH=%PYTHONPATH%;%ROOT_PATH%;%PFE_ENV%;%DEV_ENV%;%venv%
+
+"C:\Program Files\Side Effects Software\Houdini 18.0.532\bin\houdini.exe"
