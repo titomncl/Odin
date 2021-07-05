c5714166f18ba8d08c07f1edfd91c424478f4c42
titomncl <70750510+titomncl@users.noreply.github.com>
2021-07-05 15:50:02 +0200

    del dist

--- dist/Odin/softwaresList/maya.bat
+++
@@ -1,4 +0,0 @@
-set PYTHONPATH=C:\Program Files\Autodesk\Maya2019\Python\Lib\site-packages;%PYTHONPATH%
-set PYTHONPATH=%PYTHONPATH%;%ROOT_PATH%;%PFE_ENV%;%DEV_ENV%;%venv%
-
-"C:\Program Files\Autodesk\Maya2019\bin\maya.exe"
