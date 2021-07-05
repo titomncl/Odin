2980d7b5f731cf5a1e72b5dd20718327c257d5fb
titomncl <70750510+titomncl@users.noreply.github.com>
2021-07-05 17:18:36 +0200

    add softwaresList folder in VCS

---
+++ dist/Odin/softwaresList/houdini.bat
@@ -0,0 +1,4 @@
+set PYTHONPATH=%PYTHONPATH%;%ROOT_PATH%;%PFE_ENV%;%DEV_ENV%;
+set HOUDINI_TOOLBAR_PATH=%DEV_ENV%/Houdini/toolbar
+
+"C:\Program Files\Side Effects Software\Houdini 18.0.532\bin\houdini.exe"
