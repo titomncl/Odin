5816f310954e32285b858ff2dcb4d18f9de8ccff
titomncl <70750510+titomncl@users.noreply.github.com>
2021-07-05 18:50:06 +0200

    clean software.py, maya.bat and make Odin.exe a window, not a console

--- source/core/software.py
+++ source/core/software.py
@@ -3,13 +3,18 @@ import os
 
 from Odin.source.common import concat
 
+def get_launch_env(py_ver):
+    launch_env = os.environ.copy()
+
+    launch_env["PYTHONPATH"] = os.getenv("DEV_ENV") + "/venv/" + py_ver + "/Lib/site-packages;"
+
+    return launch_env
+
 def maya():
 
     maya = concat(os.getcwd().replace("\\", "/"), "softwaresList/maya.bat", separator="/")
 
-    launch_env = os.environ.copy()
-
-    launch_env["PYTHONPATH"] = os.getenv("DEV_ENV") + "/venv/27/Lib/site-packages;"
+    launch_env = get_launch_env("27")
 
     subprocess.Popen(maya, env=launch_env, cwd=r"C:\Program Files\Autodesk\Maya2019")
 
@@ -18,8 +23,6 @@ def houdini():
 
     houdini = concat(os.getcwd().replace("\\", "/"), "softwaresList/houdini.bat", separator="/")
 
-    launch_env = os.environ.copy()
-
-    launch_env["PYTHONPATH"] = os.getenv("DEV_ENV") + "/venv/27/Lib/site-packages;"
+    launch_env = get_launch_env("27")
 
     subprocess.Popen(houdini, env=launch_env, cwd=r"C:\Program Files\Autodesk\Maya2019")
