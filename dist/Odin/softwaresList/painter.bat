2a5e41ae05e2ba0558ffebc3caa1ea8b86c43cda
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-07-15 12:35:49 +0200

    PEP8 and remove files from VCS

--- dist/Odin/softwaresList/painter.bat
+++
@@ -1,5 +0,0 @@
-::set PYTHONPATH=E:/DEV;E:/DEV/venv/38/Lib/site-packages;
-set PYTHONPATH=%PYTHONPATH%;%ROOT_PATH%;%PFE_ENV%;%DEV_ENV%;
-set SUBSTANCE_PAINTER_PLUGINS_PATH=%DEV_ENV%/Painter;
-
-start "" %1
