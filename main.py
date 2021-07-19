f88d61ea35495ba6caaaf9195eb3840cf042a59d
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-07-19 23:25:30 +0200

    Odin v0.1.3:  - venv and PYTHONPATH are now local to Odin  - Add a new yaml parser  - Code cleaning  - UI cleaning for the Lib layout

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python3
+
+# Odin is a project manager and software launcher
+
 import sys
 
 from Odin.source.globals import Logger as log
 from Odin.source.controller.controller import Controller
-from Odin.source.ui.main_window import MainWindow as UI
+from Odin.source.ui.main_window import MainWindow
 from Odin.source.ui.palette import Palette
 
 from qtpy import QtWidgets
 
+__version__ = '0.1.3'
+__author__ = 'Thomas Nicole'
 
 if __name__ == '__main__':
     log.info("Init Odin")
 
     app = QtWidgets.QApplication(sys.argv)
 
-    UI_INSTANCE = Controller(UI, None)
+    UI_INSTANCE = Controller(MainWindow, None)
     Palette().set_app(app)
 
     log.info("Ready to use")
