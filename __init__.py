abc077499c6269597b837a3e7e47321b69f91da7
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-07-30 13:13:37 +0200

    0.2.0a3: Odin can be used as an API

---
+++ __init__.py
@@ -0,0 +1,7 @@
+
+__all__ = ["Project", "Asset", "Sequence", "Shot"]
+
+from .odin.source.core.project import Project
+from .odin.source.core.assets import Asset
+from .odin.source.core.sequence import Sequence
+from .odin.source.core.shot import Shot
