ef11382fa03618c5dae3d2f0553699f7bd6598d7
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-07-30 13:12:42 +0200

    del functional part of project/asset/seq/shot creation to object oriented

--- odin/source/core/assets.py
+++ odin/source/core/assets.py
@@ -8,62 +8,6 @@ except ImportError:
 from . import trees_path
 from .tree import Tree, path_from_tree
 from .yaml_parser import Parser
-from ..common import make_dirs, concat
-
-
-def create_asset(root, project, asset_name, asset_type):
-    # type: (str, str, str, str) -> bool
-    """
-    Args:
-        root (str): root path of the project without the slash at the end
-        project (str): project
-        asset_name (str): asset name
-        asset_type (str): CHARA or PROPS
-
-    Returns:
-        bool: True if the project was created, False if it was not
-
-    """
-    asset_path = concat(root, project, "DATA/LIB", asset_type.upper(), asset_name, separator="/")
-    asset_tree = Tree.create_from_template(trees_path.asset_tree(), asset_path)
-
-    asset_publish_path = concat(root, project, "DATA/LIB/PUBLISH",
-                                asset_type.upper(), asset_name, separator="/")
-    asset_publish_tree = Tree.create_from_template(trees_path.asset_publish_tree(), asset_publish_path)
-
-    asset_created = make_dirs(asset_path)
-    asset_publish_created = make_dirs(asset_publish_path)
-
-    if asset_created and asset_publish_created:
-        asset_tree.create_on_disk()
-        asset_publish_tree.create_on_disk()
-
-        return True
-    else:
-        return False
-
-
-def find_assets(root, project, type_):
-    # type: (str, str, str) -> List[str]
-    """
-    Args:
-        root (str):
-        project (str):
-        type_ (str): CHARACTER, PROPS folder
-
-    Returns:
-        list (str): assets found in the folder
-
-    """
-    if project:
-        path = concat(root, project, "DATA/LIB", type_, separator="\\")
-
-        try:
-            assets = next(os.walk(path))[1]
-
-            return assets
-        except StopIteration:
-            return list()
 
 
 class Asset(object):
--- odin/source/core/shot.py
+++ odin/source/core/shot.py
@@ -8,57 +8,6 @@ except ImportError:
 from . import trees_path
 from .tree import Tree, path_from_tree
 from .yaml_parser import Parser
-from ..common import make_dirs, concat
-
-
-def create_shot(root, project, seq, shot):
-    # type: (str, str, str, str) -> bool
-    """
-    Args:
-        root (str): root path of the project without the slash at the end
-        project (str): project
-        sequence (str): sequence name
-        shot (str): shot name
-
-    Returns:
-        bool: True if the project was created, False if it was not
-
-    """
-    shot_path = concat(root, project, "DATA/FILM", seq, shot, separator="/")
-    shot_tree = Tree.create_from_template(trees_path.shot_tree(), shot_path)
-
-    seq_created = make_dirs(shot_path)
-
-    if seq_created:
-        shot_tree.create_on_disk()
-
-        return True
-    else:
-        return False
-
-def find_shots(root, project, seq):
-    # type: (str, str, str) -> List[str]
-    """
-    Args:
-        root (str):
-        project (str):
-        seq (str):
-
-    Returns:
-        list (str): shots found in the folder
-
-    """
-
-    if seq:
-        path = concat(root, project, "DATA/FILM", seq, separator="\\")
-
-        try:
-            shots = next(os.walk(path))[1]
-            shots.sort()
-
-            return shots
-        except StopIteration:
-            return list()
 
 
 class Shot(object):
