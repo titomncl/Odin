d3f1d69d0bfb399cf7594b3b95cbd300eb19bfed
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-08-04 12:41:39 +0200

    bug with venv path

--- odin/source/controller.py
+++ odin/source/controller.py
@@ -165,7 +165,7 @@ class Controller(object):
         os.environ["ROOT_PATH"] = self.root
         os.environ["PROJECT_ENV"] = os.path.join(self.root, self.project_name)
 
-        venv = os.path.abspath("./venv").replace("\\", "/")
+        venv = os.path.realpath("../venv").replace("\\", "/")
 
         log.info("Project set: " + self.root + self.project_name)
 
