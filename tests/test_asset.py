d9398a54cd7a9a055c1873f6b3155a747caca5a0
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-08-06 00:03:38 +0200

    add test_asset.py *not finished*

--- tests/test_asset.py
+++ tests/test_asset.py
@@ -16,6 +16,10 @@ def tmp_dir(tmp_path_factory):
 def name():
     yield str(binascii.b2a_hex(os.urandom(2))).split("'")[1].upper()
 
+@pytest.fixture(scope="function")
+def asset_name():
+    yield str(binascii.b2a_hex(os.urandom(2))).split("'")[1].upper()
+
 
 @pytest.fixture(scope="module")
 def project(tmp_dir, name):
@@ -26,24 +30,34 @@ def project(tmp_dir, name):
 
     yield prj
 
+
+@pytest.fixture(scope="module")
+def asset(project, asset_name):
+    try:
+        asset = Asset.load(project, asset_name, "CHARA")
+    except RuntimeError:
+        asset = Asset.new(project, asset_name, "CHARA")
+
+
 parameters = [
-    (project, "C01", "CHARA"),
-    (project, "P01", "PROPS"),
-    (project, "S01", "SET"),
-    (project, "F01", "FX"),
+    (pytest.lazy_fixture("project"), "C01", "CHARA"),
+    (pytest.lazy_fixture("project"), "P01", "PROPS"),
+    (pytest.lazy_fixture("project"), "S01", "SET"),
+    (pytest.lazy_fixture("project"), "F01", "FX"),
 ]
 
-@pytest.mark.parametrize("prj, name, asset_type", parameters)
-def test_asset_new(prj, name, asset_type):
 
-    asset = Asset.new(prj, name, asset_type)
+@pytest.mark.parametrize("prj, asset_name, asset_type", parameters)
+def test_asset_new(prj, asset_name, asset_type):
 
-    assert asset.name == name
+    asset = Asset.new(prj, asset_name, asset_type)
+
+    assert asset.name == asset_name
     assert asset.asset_type == asset_type
-    assert name in asset.list(prj, asset_type)
+    assert asset_name in asset.list(prj, asset_type)
 
 
-def test_project_load(project, tmp_dir, name):
+def test_asset_load(asset, tmp_dir, name):
     prj = Project.load(tmp_dir, name)
 
     prj_data = os.path.join(tmp_dir, name, "odin.yaml")
