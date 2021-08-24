82e6628fd73d1c1db839a49f4174d2311d8ccf26
TitomNCL <70750510+titomncl@users.noreply.github.com>
2021-08-24 12:53:31 +0200

    ✨ Update based on user preference: beta or not. Includes the possibility to stop asking to update the soft

---
+++ odin/source/core/update_version.py
@@ -0,0 +1,60 @@
+import requests
+import re
+
+from typing import Union, List
+
+version_pattern = re.compile(r"^(?P<major>\d).(?P<minor>\d).(?P<micro>\d)b?(?P<beta>\d*)?$")
+
+
+def new_update(version, is_beta):
+    # type: (str, bool) -> Union[None, str]
+    url = "https://api.github.com/repos/titomncl/odin/tags"
+
+    r = requests.request(method="GET", url=url)
+
+    tags = r.json()
+
+    actual_version = version_pattern.match(version).groupdict()
+
+    for tag in tags:
+        last_version = version_pattern.match(tag["name"])
+
+        if last_version:
+            last_version = last_version.groupdict()
+
+            actual_v = [int(actual_version["major"]), int(actual_version["minor"]), int(actual_version["micro"])]
+            if actual_version["beta"]:
+                actual_v.append(int(actual_version["beta"]))
+
+            last_v = [int(last_version["major"]), int(last_version["minor"]), int(last_version["micro"])]
+            if last_version["beta"]:
+                last_v.append(int(last_version["beta"]))
+
+            if version_verification(actual_v, last_v, is_beta):
+                return tag["name"]
+
+
+def version_verification(actual, new, is_beta):
+    # type: (List[int], List[int], bool) -> bool
+    if len(actual) < len(new):
+        if is_beta:
+            new.pop(-1)
+            if actual > new:
+                return False
+            else:
+                return True
+        else:
+            return False
+    elif len(actual) > len(new):
+        actual.pop(-1)
+        if actual > new:
+            return False
+        else:
+            return True
+    elif len(new) == 4 and not is_beta:
+        return False
+    else:
+        if actual < new:
+            return True
+        else:
+            return False
---
+++ odin/source/ui/updater.py
@@ -0,0 +1,124 @@
+import urllib.request
+import os
+
+from qtpy import QtWidgets as Qw
+from qtpy import QtCore as Qc
+
+from odin import __version__
+from ..core.update_version import new_update
+from ..core.yaml_parser import Parser
+
+
+class Updater(Qw.QDialog):
+    def __init__(self, is_beta, parent=None):
+        Qw.QDialog.__init__(self, parent)
+
+        self.setWindowTitle("Update Odin")
+
+        self.p_bar = None
+        self.tag = new_update(__version__, is_beta)
+
+        self.updated = False
+
+        if self.tag:
+            if self.do_update():
+                self.update_soft(self.tag)
+                self.confirm_msg_box()
+                self.updated = True
+
+    def do_update(self):
+        do_update_msg_box = Qw.QMessageBox(self)
+
+        do_update_msg_box.setIcon(do_update_msg_box.Icon.Question)
+        do_update_msg_box.setWindowTitle("New update")
+        do_update_msg_box.setText("A new version of Odin is available.")
+        do_update_msg_box.setInformativeText("Do you want to download it?")
+
+        action_btn = do_update_msg_box.addButton("Yes", do_update_msg_box.ButtonRole.AcceptRole)
+        cancel_btn = do_update_msg_box.addButton("No", do_update_msg_box.ButtonRole.RejectRole)
+        later_btn = do_update_msg_box.addButton("Later", do_update_msg_box.ButtonRole.ActionRole)
+
+        do_update_msg_box.exec()
+
+        if do_update_msg_box.clickedButton() == action_btn:
+            return True
+        elif do_update_msg_box.clickedButton() == cancel_btn:
+            self.update_reminder()
+            return False
+        elif do_update_msg_box.clickedButton() == later_btn:
+            return False
+
+    def confirm_msg_box(self):
+        msg_box = Qw.QMessageBox(self)
+
+        msg_box.setIcon(msg_box.Icon.Information)
+        msg_box.setWindowTitle("Updated")
+        msg_box.setText("The new version of Odin has been downloaded here:")
+        msg_box.setInformativeText(os.path.join(os.path.expanduser("~"), "Downloads"))
+
+        action_btn = msg_box.addButton("Open", msg_box.ButtonRole.ActionRole)
+        cancel_btn = msg_box.addButton("Cancel", msg_box.ButtonRole.RejectRole)
+
+        msg_box.exec()
+
+        if msg_box.clickedButton() == action_btn:
+            os.startfile(os.path.join(os.path.expanduser("~"), "Downloads"))
+            self.close()
+        elif msg_box.clickedButton() == cancel_btn:
+            self.close()
+
+    def update_reminder(self):
+        reminder = Qw.QMessageBox(self)
+
+        reminder.setWindowTitle("Reminder")
+
+        reminder.setText("Do you want to me reminded?")
+
+        action_btn = reminder.addButton("Yes", reminder.ButtonRole.ActionRole)
+        cancel_btn = reminder.addButton("No", reminder.ButtonRole.RejectRole)
+
+        reminder.exec()
+
+        config = Parser.open("./config/config_file.yaml")
+
+        if reminder.clickedButton() == cancel_btn:
+            config.data["UPDATE"] = False
+        elif reminder.clickedButton() == action_btn:
+            config.data["UPDATE"] = True
+
+        config.write()
+
+    def progress_bar(self):
+        self.p_bar = Qw.QProgressBar()
+        self.p_bar.setFixedSize(200, 50)
+        self.p_bar.setWindowFlag(Qc.Qt.WindowType.Tool)
+
+        label = Qw.QLabel("Download Odin v{}...".format(self.tag))
+
+        layout = Qw.QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.p_bar)
+        self.setLayout(layout)
+        self.setWindowTitle("Downloading...")
+
+        self.show()
+
+    def handle_progress(self, block_num, block_size, total_size):
+        # type: (int, int, int) -> None
+        data = block_num * block_size
+
+        if total_size > 0:
+            download_percentage = data * 100 / total_size
+            self.p_bar.setValue(int(download_percentage))
+            Qw.QApplication.processEvents()
+
+    def update_soft(self, last_version):
+        # type: (str) -> None
+        self.progress_bar()
+
+        new_release_url = "https://github.com/titomncl/Odin/releases/download/" \
+                          + last_version + "/Odin_" + last_version + ".zip"
+
+        download_path = os.path.join(os.path.expanduser("~"), "Downloads", 'Odin_' + last_version + '.zip')
+
+        urllib.request.urlretrieve(new_release_url, download_path, self.handle_progress)
